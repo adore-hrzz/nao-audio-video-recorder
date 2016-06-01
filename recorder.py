1102a1606e0ddf0d79e2bd8ad078ff0e532577b9
fpetric <f5r1c.1m0@gmail.com>
2016-06-01 13:27:46 +0200

    Changed formatting and some function names, added readings from the head tactile sensors

--- recorder.py
+++ recorder.py
@@ -5,9 +5,10 @@ import tkFont
 
 
 class MainWindow(tk.Frame):
-    ''' Main Window '''
+    """ Main Window """
 
     def __init__(self, master):
+        """ Default constructor, initializes variables """
         self.master = master
 
         # initialize tk frame
@@ -30,8 +31,14 @@ class MainWindow(tk.Frame):
         self.audio_id = 0
         self.time_start = 0
 
+        self.camera_label = []
+        self.audio_label = []
+        self.label = []
+        self.log_sonar = []
+        self.log_tactile = []
+
         # create GUI
-        self.initializeGUI()
+        self.initialize_gui()
 
         # create naoqi proxies
         self.videoRecorderProxy = []
@@ -39,10 +46,8 @@ class MainWindow(tk.Frame):
         self.sonarProxy = []
         self.memoryProxy = []
 
-
-    def initializeGUI(self):
-        ''' Initializes GUI'''
-        
+    def initialize_gui(self):
+        """ Initializes GUI """
         self.master.title('NAO audio video recorder')
         self.pack(fill=tk.BOTH, expand=True)
 
@@ -50,7 +55,7 @@ class MainWindow(tk.Frame):
         frame_robot_config = tk.Frame(self)
         frame_robot_config.pack(fill=tk.X)
         tk.Button(frame_robot_config, text="Connect", command=self.connect).pack(side=tk.RIGHT, padx=5, pady=5)
-        input_font = tkFont.Font(family="Arial",size=10)
+        input_font = tkFont.Font(family="Arial", size=10)
         port = tk.Entry(frame_robot_config, width=6, font=input_font, textvariable=self.port)
         port.pack(side=tk.RIGHT, padx=5, pady=5)
         tk.Label(frame_robot_config, text="PORT").pack(side=tk.RIGHT, padx=5, pady=5)
@@ -61,32 +66,38 @@ class MainWindow(tk.Frame):
         # frame for camera and audio format selection
         frame_camera_config = tk.Frame(self)
         frame_camera_config.pack(fill=tk.X)
-        self.camera_label = tk.Label(frame_camera_config, text="Top", width = 8)
+        self.camera_label = tk.Label(frame_camera_config, text="Top", width=8)
         self.camera_label.pack(side=tk.RIGHT, padx=5, pady=5)
-        tk.Button(frame_camera_config, text="Switch camera", command=self.switchCamera).pack(side=tk.RIGHT, padx=5, pady=5)
-        self.audio_label = tk.Label(frame_camera_config, text = ".wav")
+        tk.Button(frame_camera_config, text="Switch camera",
+                  command=self.switch_camera).pack(side=tk.RIGHT, padx=5, pady=5)
+        self.audio_label = tk.Label(frame_camera_config, text=".wav")
         self.audio_label.pack(side=tk.RIGHT, padx=5, pady=5)
-        tk.Button(frame_camera_config, text="Switch audio", command=self.switchAudio).pack(side=tk.RIGHT, padx=5, pady=5)       
+        tk.Button(frame_camera_config, text="Switch audio",
+                  command=self.switch_audio).pack(side=tk.RIGHT, padx=5, pady=5)
         
         # additional video label selection
         frame_video_label = tk.Frame(self)
         frame_video_label.pack(fill=tk.X)
-        tk.Button(frame_video_label, text='Clear', command=self.clear_label, width = 10).pack(side=tk.RIGHT, padx=5, pady=5)
-        tk.Entry(frame_video_label, width=20, font=input_font, textvariable=self.video_label).pack(side=tk.RIGHT, padx=5, pady=5)
+        tk.Button(frame_video_label, text='Clear', command=self.clear_label,
+                  width = 10).pack(side=tk.RIGHT, padx=5, pady=5)
+        tk.Entry(frame_video_label, width=20, font=input_font,
+                 textvariable=self.video_label).pack(side=tk.RIGHT, padx=5, pady=5)
         tk.Label(frame_video_label, text="Video label").pack(side=tk.RIGHT, padx=5, pady=5)
 
         # checkboxes for sonar and tactile sensors readings
         frame_checkboxes = tk.Frame(self)
         frame_checkboxes.pack(fill=tk.X)
-        tactile = tk.Checkbutton(frame_checkboxes, text="Record tactile sensors", variable=self.recordTactile).pack(side=tk.RIGHT, padx=5, pady=5)
-        sonar = tk.Checkbutton(frame_checkboxes, text="Record sonar readings", variable=self.recordSonar).pack(side=tk.RIGHT, padx=5, pady=5)
+        tk.Checkbutton(frame_checkboxes, text="Record tactile sensors",
+                       variable=self.recordTactile).pack(side=tk.RIGHT, padx=5, pady=5)
+        tk.Checkbutton(frame_checkboxes, text="Record sonar readings",
+                       variable=self.recordSonar).pack(side=tk.RIGHT, padx=5, pady=5)
 
         # frame for record and stop buttons
         frame_buttons = tk.Frame(self)
         frame_buttons.pack(fill=tk.X)
         start_button = tk.Button(frame_buttons, text='Start recording', command=self.start)
         stop_button = tk.Button(frame_buttons, text='Stop recording', command=self.stop)
-        close_button = tk.Button(frame_buttons, text='Close', command=self.close, width = 10)
+        close_button = tk.Button(frame_buttons, text='Close', command=self.close, width=10)
         close_button.grid(row=0, column=2, padx=5, pady=5)
         stop_button.grid(row=0, column=1, padx=5, pady=5)
         start_button.grid(row=0, column=0, padx=5, pady=5)
@@ -98,11 +109,11 @@ class MainWindow(tk.Frame):
         self.label.pack(fill=tk.X)
 
     def clear_label(self):
+        """ Clears label for the video """
         self.video_label.set("")
         
     def connect(self):
-        ''' Connect with the robot '''
-        
+        """ Connect with the robot """
         # try to establish a connection
         try:        
             self.videoRecorderProxy = ALProxy("ALVideoRecorder", self.ip.get(), self.port.get())
@@ -120,25 +131,24 @@ class MainWindow(tk.Frame):
             self.isConnected = False
             self.label.config(text='Not connected')
 
-    def switchCamera(self):
-        ''' Change the recording device on the robot '''
-        
+    def switch_camera(self):
+        """ Change the recording device on the robot """
         # switch camera if connected
         if self.isConnected and not self.isRecordingVideo:
             self.videoRecorderProxy.setCameraID(1 - self.videoRecorderProxy.getCameraID())
             self.camera_label.config(text=self.camera_dict[self.videoRecorderProxy.getCameraID()])
 
-    def switchAudio(self):
-        ''' Change the format of audio recording
+    def switch_audio(self):
+        """ Change the format of audio recording
             .ogg is a single channel recording from the front microphone
             .wav (default) is a 4-channel recording from all microphones
-        '''
+        """
         if not self.isRecordingAudio:
             self.audio_id = 1 - self.audio_id
             self.audio_label.config(text=self.audio_dict[self.audio_id])
 
     def start(self):
-        ''' Start recording if connected'''
+        """ Start recording if connected """
         if not self.isConnected:
             self.label.config(text='Not connected')
             return
@@ -162,18 +172,16 @@ class MainWindow(tk.Frame):
             self.log_tactile = open('./sensor_readings/'+filename+'_tactile.txt', 'w')
         self.time_start = time.time()
 
-
     def stop(self):
-        ''' Stop recording if connected and already recording '''
-        
+        """ Stop recording if connected and already recording """
         if not self.isConnected:
             self.label.config(text='Not connected')
             return
         if self.isRecordingVideo:
-            video_info = self.videoRecorderProxy.stopRecording()
+            self.videoRecorderProxy.stopRecording()
             self.isRecordingVideo = False
         if self.isRecordingAudio:
-            audio_info = self.audioRecorderProxy.stopMicrophonesRecording()
+            self.audioRecorderProxy.stopMicrophonesRecording()
             self.isRecordingAudio = False
         if not self.isRecordingAudio and not self.isRecordingVideo:
             self.label.config(text='Recording stopped')
@@ -186,8 +194,7 @@ class MainWindow(tk.Frame):
             self.log_tactile.close()
 
     def close(self):
-        ''' Stop recording and close the program '''
-
+        """ Stop recording and close the program """
         if self.isRecordingVideo:
             self.videoRecorderProxy.stopRecording()
         if self.isRecordingAudio:
@@ -195,6 +202,7 @@ class MainWindow(tk.Frame):
         self.master.destroy()
 
     def to_do(self):
+        """ Do stuff while in the TK main loop """
         time_stamp = time.time()-self.time_start
         if self.isRecordingSonar:
             val_left = self.memoryProxy.getData("Device/SubDeviceList/US/Left/Sensor/Value")
@@ -207,12 +215,18 @@ class MainWindow(tk.Frame):
             val_right1 = str(self.memoryProxy.getData("HandLeftLeftTouched"))
             val_right2 = str(self.memoryProxy.getData("HandLeftBackTouched"))
             val_right3 = str(self.memoryProxy.getData("HandLeftRightTouched"))
-            self.log_tactile.write(str(time_stamp)+','+val_left1+','+val_left2+','+val_left3+','+val_right1+','+val_right2+','+val_right3+'\n')
+            val_head1 = str(self.memoryProxy.getData("FrontTactilTouched"))
+            val_head2 = str(self.memoryProxy.getData("MiddleTactilTouched"))
+            val_head3 = str(self.memoryProxy.getData("RearTactilTouched"))
+            self.log_tactile.write(str(time_stamp)+',' +
+                                   val_left1 + ',' + val_left2 + ','+val_left3 + ',' +
+                                   val_right1 + ',' + val_right2 + ',' + val_right3 + ',' +
+                                   val_head1 + ',' + val_head2 + ',' + val_head3 + '\n')
         self.master.after(10, self.to_do)
 
+
 def main():
-    ''' Main function '''
-    
+    """ Main function """
     # create tkinter window
     root = tk.Tk()
     main_window = MainWindow(root)
